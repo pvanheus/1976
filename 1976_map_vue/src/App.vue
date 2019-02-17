105a549111be568ae02ccc3c59e2dac47b4ee2b9
Peter van Heusden <pvh@sanbi.ac.za>
2019-02-17 19:42:25 +0200

    Move towards Vue

---
+++ 1976_map_vue/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="row">
+    <div class="col s8 offset-s2">
+      <Header />
+      <Map />
+    </div>
+  </div>
+</template>
+
+<script>
+import Header from "./components/Header";
+import Map from "./components/Map";
+
+export default {
+  name: 'MainPage',
+  components: {
+    Header,
+    Map
+  }
+};
+</script>
+
+<style></style>
---
+++ 1976_map_vue/src/components/Header.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col s8 offset-s2">
+        <div id="header">
+          <h4>In memory of 1976</h4>
+          <p>
+            June 16 1976 marks the start of the 1976 youth uprising against
+            Apartheid in South Africa. Most closely associated with the school
+            students march in Soweto and the murder of Hector Pieterson, the
+            revolt spread across the country and well past June 1976. Along with
+            the revolt came the deaths, most often of those shot by police. The
+            South African History Online website has tried to collect a
+            <a
+              href="http://www.sahistory.org.za/topic/june-16-soweto-youth-uprising"
+              >list</a
+            >
+            of the "casaulties of 1976". I have taken that list, done a little
+            cleanup (code is
+            <a href="https://github.com/pvanheus/1976">here</a>) and produced
+            the map below.
+          </p>
+        </div>
+        <div id="map" class="map"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped></style>
